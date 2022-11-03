621336f0ec4eb9a4b88440e979322ac808e5fdd7
Apriyadi Hegarto <ahegarto@gmail.com>
2022-11-03 22:46:42 +0700

    sync with VS

---
+++ LXIxSpartaBootCamp/5.week3/React_Mastery Test Question/todolist_redux_assignment 4/src/redux/modules/todos.js
@@ -0,0 +1,120 @@
+// Action value
+const ADD_TODO = "ADD_TODO";
+const GET_TODO_BY_ID = "GET_TODO_BY_ID";
+const DELETE_TODO = "DELETE_TODO";
+const TOGGLE_STATUS_TODO = "TOGGLE_STATUS_TODO";
+
+// Action Creator
+// Todo를 추가하는 action creator
+export const addTodo = (payload) => {
+  return {
+    type: ADD_TODO,
+    payload,
+  };
+};
+
+// Todo를 지우는 action creator
+export const deleteTodo = (payload) => {
+  return {
+    type: DELETE_TODO,
+    payload,
+  };
+};
+
+// Todo를 isDone를 변경하는 action creator
+export const toggleStatusTodo = (payload) => {
+  return {
+    type: TOGGLE_STATUS_TODO,
+    payload,
+  };
+};
+
+// 상세 페이지에서 특정 Todo만 조회하는 action creator
+export const getTodoByID = (payload) => {
+  return {
+    type: GET_TODO_BY_ID,
+    payload,
+  };
+};
+
+// initial state
+const initialState = {
+  todos: [
+    {
+      id: "1",
+      title: "리액트",
+      body: "리액트를 배워봅시다",
+      isDone: false,
+    },
+  ],
+  todo: {
+    id: "0",
+    title: "",
+    body: "",
+    isDone: false,
+  },
+};
+
+const todos = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return {
+        ...state,
+        // old code
+        // todos: [action.payload],
+
+        // New code
+        todos : [...state.todos,action.payload],
+      };
+
+    // New Code
+    case DELETE_TODO:
+      const todos_sementara_hapus = state.todos.filter(todo => {
+        return todo.id !== action.payload
+      })
+      return {
+        ...state,
+        todos: todos_sementara_hapus,
+      };
+
+    case TOGGLE_STATUS_TODO:
+      // new code
+      const temp_todos_toggle = state.todos.map((todo)=>{
+        if (todo.id === action.payload) {
+            return ({...todo, isDone: !todo.isDone});
+        } 
+        return todo;  
+      });
+      return {
+        ...state,
+        todos: temp_todos_toggle,
+      };
+
+      // old code
+      // return {
+      //   ...state,
+      //   todos: state.todos.map((todo) => {
+      //     if (todo.id === action.payload) {
+      //       return {
+      //         ...todo,
+      //         isDone: !todo.isDone,
+      //       };
+      //     } else {
+      //       return todo;
+      //     }
+      //   }),
+      // };
+
+    case GET_TODO_BY_ID:
+      return {
+        ...state,
+        todo: state.todos.find((todo) => {
+          return todo.id === action.payload;
+        }),
+      };
+    default:
+      return state;
+  }
+};
+
+export default todos;
